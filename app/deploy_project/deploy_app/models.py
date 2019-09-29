0e97e939b9e8862ec4d875f0c37ac5a25a929c20
Sean Burtner <seb4jw@virginia.edu>
2019-09-29 16:34:16 -0400

    models and migrate

--- app/deploy_project/deploy_app/models.py
+++ app/deploy_project/deploy_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class SeatingSection(models.Model):
+	ID = models.IntegerField()
+class Table(models.Model):
+	tableNumber = models.IntegerField()
+	section = models.ForeignKey(SeatingSection, on_delete=models.CASCADE)
+
+class StaffMember(models.Model):
+	firstName = models.CharField(max_length=15)
+	ID = models.IntegerField()
+	working = models.BooleanField(default=False)
+
+class Chef(StaffMember):
+	headChef = models.BooleanField(default=False)
+
+class Server(StaffMember):
+	currentTables = models.CharField(max_length=14)
+
+class Support(StaffMember):
+	POSITION = [
+		('H', 'Host'),
+		('FR', 'Food Runner'),
+		('B', 'Busser'),
+	]
+	position = models.CharField(max_length = 2, choices=POSITION)
